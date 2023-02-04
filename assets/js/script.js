9203337f055d7e3714c5ca0b224ae9754924440a
Terri Mack <terrimack15@gmail.com>
2023-02-03 20:04:18 -0800

    added variables in java

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// api key code
+var apiKey = '4592844a9d1c589bd9c6d1289e2fe7fb'
+
+// variables
+var cityInput = document.querySelector('#enterCity');
+var searchBtn = document.querySelector('.searchBtn');
+var searchHistoryContainer = document.querySelector('.history-container');
+var searchHistory = document.querySelector('.search-history');
+var forecastContainer = document.querySelector('.5-dayForecast');
+
