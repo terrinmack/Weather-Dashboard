613dac8144d97a85b1e10ffc4bd5166b93b309aa
Terri Mack <terrimack15@gmail.com>
2023-02-04 21:13:45 -0800

    resetting html for a more organized look. setting up page with bootstrap organization instead

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,27 +11,21 @@ var searchHistoryEl = document.querySelector('.search-history');
 var currentCityEl = document.querySelector('.currentCity-container');
 var forecastContainerEl = document.querySelector('.five-dayForecast');
 
+var cityURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
 var cityName = data.name;
 var latitute = data.coord.lat;
 var longitude = data.coord.lon;
 
 
 // function to call the city name and get info
-var getCityInfo = function (city) {
-        var cityURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
-
-        fetch(cityURL)
-            .then(function(response) {
-                if (response.ok) {
-                    console.log(response)
-                    response.json().then(function(data) {
-                        console.log(data);
-                        displayWeather(data, city)
-                    });
-                } else {
-                    alert('Please enter a city name')
-                }
-            });
-        };
-
-// event listener for form submit
+    
+function getWeather(city) {
+    fetch(cityURL)
+    .then (function(response) {
+        console.log(response);
+        response.json().then(function (data) {
+            console.log(data);
+            displayWeather(data, city);
+        });
+    })
+}
