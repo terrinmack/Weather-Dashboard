e17f4525c8777621bea4a33ff4bb10ac4181bbbf
Terri Mack <terrimack15@gmail.com>
2023-02-04 20:36:00 -0800

    edited current city container in html and added variables in script

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,39 +2,36 @@
 var apiKey = '4592844a9d1c589bd9c6d1289e2fe7fb'
 
 // variables
+var searchFormEl = document.querySelector('.search-form');
 var cityInput = document.querySelector('#enterCity');
+var cityName = document.querySelector('.city-name');
 var searchBtn = document.querySelector('.searchBtn');
 var searchHistoryContainerEl = document.querySelector('.history-container');
 var searchHistoryEl = document.querySelector('.search-history');
-var currentCityEl = document.querySelector('.currentCity-container')
+var currentCityEl = document.querySelector('.currentCity-container');
 var forecastContainerEl = document.querySelector('.five-dayForecast');
 
-// search function
-var formSubmitHandler = function(event) {
-    event.preventDefault();
+var cityName = data.name;
+var latitute = data.coord.lat;
+var longitude = data.coord.lon;
 
-    var cityName = cityInput.value.trim();
 
-    if (cityName) {
-        getCityInfo(cityName);
-
-        currentCityEl.innerHTML = '';
-        cityInput.value = '';
-    } else {
-        alert('Please enter a city name')
-    }
-};
-
+// function to call the city name and get info
 var getCityInfo = function (city) {
         var cityURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
 
-        fetch(cityURL).then(function(response) {
-            response.json().then(function(data){
-                console.log(data);
+        fetch(cityURL)
+            .then(function(response) {
+                if (response.ok) {
+                    console.log(response)
+                    response.json().then(function(data) {
+                        console.log(data);
+                        displayWeather(data, city)
+                    });
+                } else {
+                    alert('Please enter a city name')
+                }
+            });
+        };
 
-                var cityName = data.name;
-                var latitute = data.coord.lat;
-                var longitude = data.coord.lon;
-            })
-        })
-    }
+// event listener for form submit
