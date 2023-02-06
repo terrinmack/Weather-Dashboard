a5109e23745b3ab10248d136de8eb8d797bbde91
Terri Mack <terrimack15@gmail.com>
2023-02-05 20:56:23 -0800

    java code did not work. restarting

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,6 @@
 var apiKey = '4592844a9d1c589bd9c6d1289e2fe7fb'
 
 // variables
-var searchHistory = [];
 var cityInput = document.querySelector('#enterCity');
 var searchBtn = document.querySelector('.searchBtn');
 var historyContainer = document.querySelector('searchHistory');
@@ -14,25 +13,3 @@ var currentUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '
 var forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + apiKey;
 
 
-
-
-
-
-
-
-
-
-
-
-
-// creating local storage history list
-for (var i = 0; i < localStorage.length; i++) {
-    var cityHistory = document.createElement('li');
-    cityHistory.addClass('historyList');
-
-    historyContainer.appendChild(cityHistory)
-}
-
-
-
-searchBtn.addEventListener('click', formSubmit);
