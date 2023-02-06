61072a090a91371efbd5a17fc78629463292f5ab
Terri Mack <terrimack15@gmail.com>
2023-02-05 22:18:59 -0800

    editing javascript file from start

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-// api key code
-var apiKey = '4592844a9d1c589bd9c6d1289e2fe7fb'
 
 // variables
 var cityInput = document.querySelector('#enterCity');
@@ -8,8 +6,16 @@ var historyContainer = document.querySelector('searchHistory');
 var currentContainer = document.querySelector('.currentWeather');
 var forecastContainer = document.querySelector('.five-day');
 
-var currentUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
+var currentUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityInput.value + '&appid=4592844a9d1c589bd9c6d1289e2fe7fb'
 
-var forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + apiKey;
+var forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityInput.value + '&appid=4592844a9d1c589bd9c6d1289e2fe7fb';
 
-
+searchBtn.addEventListener('click', function() {
+fetch(currentUrl)
+    .then(function(response) {
+        response.json()
+        .then(function(data) {
+            console.log(data);
+        })
+    })
+})
