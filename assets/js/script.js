28842d3bf8203ded74ab5d13a0935929fc285068
Terri Mack <terrimack15@gmail.com>
2023-02-05 20:40:04 -0800

    added a local storage list item code in java

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,23 +2,37 @@
 var apiKey = '4592844a9d1c589bd9c6d1289e2fe7fb'
 
 // variables
+var searchHistory = [];
+var cityInput = document.querySelector('#enterCity');
+var searchBtn = document.querySelector('.searchBtn');
+var historyContainer = document.querySelector('searchHistory');
+var currentContainer = document.querySelector('.currentWeather');
+var forecastContainer = document.querySelector('.five-day');
+
+var currentUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
+
+var forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + apiKey;
 
 
-var cityURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
-var cityName = data.name;
-var latitute = data.coord.lat;
-var longitude = data.coord.lon;
 
 
-// function to call the city name and get info
-    
-function getWeather(city) {
-    fetch(cityURL)
-    .then (function(response) {
-        console.log(response);
-        response.json().then(function (data) {
-            console.log(data);
-            displayWeather(data, city);
-        });
-    })
+
+
+
+
+
+
+
+
+
+// creating local storage history list
+for (var i = 0; i < localStorage.length; i++) {
+    var cityHistory = document.createElement('li');
+    cityHistory.addClass('historyList');
+
+    historyContainer.appendChild(cityHistory)
 }
+
+
+
+searchBtn.addEventListener('click', formSubmit);
